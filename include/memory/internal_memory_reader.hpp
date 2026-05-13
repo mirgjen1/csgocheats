0bdb872ed93f05ed96f901b744539865b177be0f
mirgjengashi <mirgjen.gashi5@gmail.com>
2026-05-13 09:34:04 -0400

    refactor: update read_memory to return bytes read and remove templated read method

--- include/memory/internal_memory_reader.hpp
+++ include/memory/internal_memory_reader.hpp
@@ -10,25 +10,14 @@ class InternalMemoryReader : public MemoryReader {
 public:
     InternalMemoryReader() = default;
     
-    bool read_memory(uintptr_t address, void* buffer, size_t size) override {
-        if (address == 0 || buffer == nullptr) return false;
+    size_t read_memory(uintptr_t address, void* buffer, size_t size) override {
+        if (address == 0 || buffer == nullptr) return 0;
         
         // In an internal cheat, we can just use memcpy
-        // Note: In some cases, we might want to use a SEH/signal handler 
-        // to prevent crashing on invalid addresses, but for now we'll assume
-        // the addresses are valid.
         std::memcpy(buffer, reinterpret_cast<void*>(address), size);
-        return true;
+        return size;
     }
     
-    template<typename T>
-    T read(uintptr_t address) {
-        T value;
-        if (read_memory(address, &value, sizeof(T))) {
-            return value;
-        }
-        return T();
-    }
 };
 
 using InternalMemoryReaderPtr = std::shared_ptr<InternalMemoryReader>;
